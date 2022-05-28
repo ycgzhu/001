9a184bff5694dd64744503e63506a2adef84c0e7
ycgzhu <ycgzhu@gmail.com>
2022-05-28 15:33:36 +0800

    001

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   String str =Console.ReadLine();
+            if (str == "hello world")
+            {
+                Console.WriteLine("Hello <你的名字>!");
+            }
+
+
+            
+        }
+    }
+}
